c8f728f55388cea41a69e29d6d367e5278036f7c
Dytrich NguyeN <nguyen.dytrich@gmail.com>
2021-01-05 21:29:52 -0800

    Set up basic websocket

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,19 @@
-function helloWorld() {
-  console.log('Hello world!');
-}
+import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
 
-helloWorld();
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ port: 8080 });
+
+app.get('/', (req, res) => {
+  res.send('hello world.');
+});
+
+wss.on('connection', (ws) => {
+  console.log('opened websocket connection');
+});
+
+server.listen(3000, () => {
+  console.log('listening on 3000');
+});
